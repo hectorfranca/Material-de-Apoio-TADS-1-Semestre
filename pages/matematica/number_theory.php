65ff290f3ab8c172b50a4d755225abc64096868d
hectorfranca <hectorjuliomartinsdefranca@hotmail.com>
2021-10-25 14:15:32 -0300

    update number theory

--- pages/matematica/number_theory.php
+++ pages/matematica/number_theory.php
@@ -31,16 +31,20 @@
                     <a href="https://youtu.be/8guZB85DYF8" target="_blank">https://youtu.be/8guZB85DYF8</a></p>
                 <p><b>Divisibilidade:</b><br>
                     <a href="https://www.youtube.com/watch?v=llXLpnOdtMw" target="_blank">https://www.youtube.com/watch?v=llXLpnOdtMw</a></p>
+                <p><b>Máximo Divisor Comum (MDC):</b><br>
+                    <a href="https://www.youtube.com/watch?v=BKaxAFAPuS4" target="_blank">https://www.youtube.com/watch?v=BKaxAFAPuS4</a></p>
+                <p><b>Mínimo Múltiplo Comum (MMC):</b><br>
+                    <a href="https://www.youtube.com/watch?v=qPd9PnJSPd8" target="_blank">https://www.youtube.com/watch?v=qPd9PnJSPd8</a></p>
                 <p><b>Divisão Euclidiana:</b><br>
                     <a href="https://youtu.be/kyJpP_-_L_k" target="_blank">https://youtu.be/kyJpP_-_L_k</a></p>
                 <p><b>Crivo de Eratóstenes:</b><br>
                     <a href="https://www.youtube.com/watch?v=aOiJvoNX3y0" target="_blank">https://www.youtube.com/watch?v=aOiJvoNX3y0</a></p>
-                <p><b>Decomposição de números em fatores primos:</b><br>
-                    <a href="https://www.youtube.com/watch?v=asXpgcHu6DQ" target="_blank">https://www.youtube.com/watch?v=asXpgcHu6DQ</a></p>
+                <p><b>Decomposição em Fatores Primos:</b><br>
+                    <a href="https://www.youtube.com/watch?v=hVP6Xr5EcA8" target="_blank">https://www.youtube.com/watch?v=hVP6Xr5EcA8</a></p>
                 <p><b>Número de divisores:</b><br>
                     <p>1-<br>
                         <a href="https://www.youtube.com/watch?v=S8S7ppGNk48" target="_blank">https://www.youtube.com/watch?v=S8S7ppGNk48</a></p>
-                    <p>2-<br>
+                    <p>Exemplo de exercício (Determinar o valor de "n"):<br>
                         <a href="https://youtu.be/_GmG-b3fpb0?t=559" target="_blank">https://youtu.be/_GmG-b3fpb0?t=559</a></p>
                 </p>
                 <p><b>Identificar números primos:</b><br>
